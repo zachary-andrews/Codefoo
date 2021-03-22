b3d2492ed71ac8751383bc89ad3de3f95f5de709
zach.andrews <zach.andrews@madwiremedia.com>
2021-03-22 08:45:20 -0600

    updated subrectangle answer

--- Medium/Subrectangle/SubrectangleQueries.py
+++ Medium/Subrectangle/SubrectangleQueries.py
@@ -4,12 +4,12 @@ class SubrectangleQueries:
         self.rectangle = rectangle
 
     def updateSubrectangle(self, row1: int, col1: int, row2: int, col2: int, newValue: int) -> None:
-        for i,row in enumerate(self.rectangle):
-            if i >= row1 and i <= row2:
-                for j in range(len(row)):
-                    if j >= col1 and j <= col2:
-                        self.rectangle[i][j] = newValue
+        for i in range(row1,row2+1):
+            for j in range(col1, col2+1):
+                self.rectangle[i][j] = newValue
+        
         return None
+
 
     def getValue(self, row: int, col: int) -> int:
         return self.rectangle[row][col]
